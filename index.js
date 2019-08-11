f2f75b324497d2f93fd8116dd9a896d731fc68b0
digi-chris <chris@barnard.tech>
2019-08-11 18:24:32 +0100

    Initial commit
    Experimental test code at this stage.

---
+++ index.js
@@ -0,0 +1,116 @@
+var HID = require('node-hid');
+var BufferReader = require('buffer-reader');
+var devices = HID.devices();
+const os = require('os');
+
+//console.log(devices);
+var devicePath;
+for(var i = 0; i < devices.length; i++) {
+  console.log(devices[i]);
+  if(devices[i].vendorId === 7896 && devices[i].productId === 14) {
+    devicePath = devices[i].path;
+  }
+}
+
+if(devicePath) {
+  var device = new HID.HID(devicePath);
+
+  console.log(device);
+
+  //device.on("data", function(data) {
+  //  console.log("Data");
+  //  console.log(data);
+  //});
+
+  //console.log(device.readSync());
+
+  var handshake = Array(64).fill(0x00);
+  handshake[1] = 0xC3;
+  sendBuffer(handshake);
+  console.log("Handshake: " + new Buffer.from(device.readSync()).toString('ascii'));
+
+  var handshake2 = Array(64).fill(0x00);
+  handshake2[0] = 0x1A;
+  handshake2[1] = 0xC1;
+  sendBuffer(handshake2);
+  console.log("Handshake 2: " + new Buffer.from(device.readSync()).toString('ascii'));
+
+
+  var handshake3 = Array(64).fill(0x00);
+  handshake3[0] = 0xFF;
+  handshake3[1] = 0xC1;
+
+  var effectsTypes = {
+    0 : { id: 0, name: "unknown" },
+    1 : { id: 1, name: "unknown" },
+    2 : { id: 2, name: "amplifier" },
+    3 : { id: 3, name: "distortion" },
+    4 : { id: 4, name: "modulation" },
+    5 : { id: 5, name: "delay" },
+    6 : { id: 6, name: "reverb" }
+  }
+
+  // fill in unknown effects types
+  for(var i = 0; i < 255; i++) {
+    if(!effectsTypes[i]) {
+      effectsTypes[i] = { id: i, name: "unknown" };
+    }
+  }
+
+  var parameterTypes = {
+    0 : { id: 0, name: "unknown" },
+    1 : { id: 1, name: "percent" },
+    12 : { id: 12, name: "potentiometer" }
+  }
+
+  // fill in unknown effects types
+  for(var i = 0; i < 65537; i++) {
+    if(!parameterTypes[i]) {
+      parameterTypes[i] = { id: i, name: "unknown" };
+    }
+  }
+
+  var devices = {
+    11 : { name: "reverb", controls: ["amount"] },
+    22 : { name: "delay", controls: ["amount", "tap"] },
+    106 : { name: "amplifier", controls: ["volume", "gain", "unknown", "unknown", "treble", "unknown", "bass" ] }
+  }
+  
+
+  var messages = {
+    0 : { name: "Handshake" },
+    1 : { name: "Version" },
+    5 : { name: "ControlParameter", process: (flags, reader) => { return { effectType: effectsTypes[reader.nextUInt8()], deviceId: reader.nextUInt16LE(), index: reader.nextUInt8(), controlIndex: reader.nextUInt8(), parameterType: parameterTypes[reader.nextUInt16LE()], parameterValue: reader.nextUInt8() }; } },
+    6 : { name: "EffectsUnit" },
+    8 : { name: "Preset?" },
+    28: { name: "Preset", process: (flags, reader) => { return {  }}},
+    29: { name: "RotarySwitch", process: (flags, reader) => { return { position: reader.nextUInt8(), knobIndex: reader.nextUInt8() }; } }
+  };
+
+
+  function sendBuffer(buffer) {
+      if(os.platform() === "win32") {
+          buffer.unshift(0);
+      }
+      device.write(buffer);
+  }
+
+  device.on("data", function(data) {
+      //console.log(data);
+      var reader = new BufferReader(data);
+      var msgId = reader.nextUInt8();
+      var flags = reader.nextUInt8();
+      if(messages[msgId]) {
+          console.log(messages[msgId].name);
+          if(messages[msgId].process) {
+              console.log(messages[msgId].process(flags, reader));
+          } else {
+              console.log("Cannot process message.");
+          }
+      } else {
+          console.log("Unknown message: " + msgId);
+      }
+  });
+
+  sendBuffer(handshake3);
+}
